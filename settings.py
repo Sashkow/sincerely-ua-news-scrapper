7323fdeb91af8a04ffde34aa9c950d3cecd7ba56
LizaHavrylenko <35296516+LizaHavrylenko@users.noreply.github.com>
2018-03-20 22:33:09 +0200

    change settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# settings = {
+ # settings = {
 #     "www.segodnya.ua":
 #         {
 #         "articles_list_by_day_template" : "https://www.segodnya.ua/allnews/archive/{date}/p{page_number}.html",
@@ -77,7 +77,7 @@ settings = {
        "article_link_template" : "a.href",
        "date_template" : "%Y_%B_%d",
        # "pages_template" : "div.col-12",
-       "article_title_template" : "h1.itemprop",
+       "article_title_template" : "header h1", # article title template was fixed
        "article_publication_datetime" : "div.article div span:date",
        "article_text_template" : "div.block-post-text",
        "tags_template2" : "div.keywords_block", #loop through a
@@ -96,7 +96,155 @@ settings = {
        "tags_template2" : "div.tags", #loop through a
        "tags_template": "",
    }
-
+   "www.censor.net.ua":
+    {
+       "articles_list_by_day_template" : "https://censor.net.ua/news/all/page/{page_number}/archive/{date}/category/0/sortby/date",
+       "articles_list_content_blocks_template" : "div.curpane",
+       "article_link_template" : "h3 a",
+       "date_template" : "%Y-%m-%d",
+      # "pages_template" : "div.col-12",
+       "article_title_template" : "h1.entry-title",
+       "article_publication_datetime" : "time.published.dateline",
+       "article_text_template" : "div.text",
+       "tags_template2" : "div.tags", 
+       "tags_template": "div.news_by_theme",
+   },
+    "www.gordonua.com":
+    {
+        "articles_list_by_day_template" : "http://gordonua.com/html/newsitemap/%Y-%m/{date}.html",
+        "articles_list_content_blocks_template" : "body",
+        "article_link_template" :"body>a", 
+        "date_template" : "%Y-%m-%d",
+        # "pages_template" : "div.col-12",
+        "article_title_template" : "h1.a_head.flipboard-title",
+        "article_publication_datetime" :"div.a_footer time.flipboard-date",
+        "article_text_template" :"div.block.article",
+        #"tags_template2" : "", 
+       "tags_template": "div.tags.flipboard-endArticle"
+    },
+    "www.unian.ua":
+    {
+        "articles_list_by_day_template" : " https://www.unian.ua/news/archive/{date}",
+        "articles_list_content_blocks_template" : "div.publications-archive",
+        "article_link_template" : "a.publication-title",
+        "date_template" : "%Y%m%d", 
+        #"pages_template" : "div.pagination ul.pages li:last",
+        "article_title_template" : "div.article-text h1",
+        "article_publication_datetime" : "div.item.time.no-padding",
+        "article_text_template" : " h2, div.clearfix",
+        "tags_template2" : "div.mp-level.main-menu", # access to all the topics on the page, no tags
+        #"tags_template": "meta[name=keywords]",
+        #"news_keywords": "meta[name=news_keywords]"
+    },
+    "www.comments.ua":
+    {
+        "articles_list_by_day_template" : "https://comments.ua/archive/{date}/",
+        "articles_list_content_blocks_template" : "div.archive_feed_box",
+        "article_link_template" : "a.link_search_result_title",
+        "date_template" : "%Y-%m-%d",
+        #"pages_template" : "div.pagination ul.pages li:last",
+        "article_title_template" : "h1.text_news_header_text",
+        "article_publication_datetime" : "p.text_news_header_date",
+        "article_text_template" : "div#hypercontext",
+        "tags_template2" : "p.text_news_source",
+       # "tags_template": "meta[name=keywords]",
+       # "news_keywords": "meta[name=news_keywords]"
+    },
+    "www.112.ua":
+    {
+        "articles_list_by_day_template" :  "https://112.ua/archive?date_from={date}-10&date_to={date}",
+        "articles_list_content_blocks_template" : "ul.news-list",
+        "article_link_template" : "li p a",
+        "date_template" : "%Y-%m-%d",
+        "pages_template" : "ul.pagination [class!="next"] li:last", 
+        "article_title_template" : "div.b-center-item-head-info h1",
+        "article_publication_datetime" : "div.meta-info div.time",
+        "article_text_template" : "h3.article-lead, div.article-text",
+        "tags_template2" : "div.article-tags",
+        #"tags_template": "meta[name=keywords]",
+        #"news_keywords": "meta[name=news_keywords]" 
+    },
+ 
+    "www.gazeta.ua"{
+        "articles_list_by_day_template" : "https://gazeta.ua/news/{date}/", # concatenating #100 to the url does not change news displayed and sometimes leads to no news displayed at all or even 404
+        "articles_list_content_blocks_template" : "div.news-wrapper",
+        "article_link_template" : "a.news-title.block.black.fs16.mb5",
+        "date_template" : "%Y-%m-%d",
+        "pages_template" : "a#stream-next",                                 # button "Гортати далі", seems to load next 20 news on press.
+        "article_title_template" : "article h1",
+        "article_publication_datetime" : "div.pull-right.news-date",
+        "article_text_template" : "section.article-content.clearfix article",
+       "tags_template2" : "article a.w-tag-title,a.w-title",
+       "tags_template": "article a.w-title",
+       #"news_keywords": "meta[name=news_keywords]"
+    },
+    "wwww.fakty.ua":
+    {
+        "articles_list_by_day_template" : "http://fakty.ua/archive/index?d={date}&ArticlesItem_page={page_number}",
+        "articles_list_content_blocks_template" : "div.items",
+        "article_link_template" : "a.tit",
+        "date_template" : "%Y%m%d",
+        "pages_template" : "div.pager li.last a",
+        "article_title_template" : "div.zag3 h1",
+        "article_publication_datetime" : "span.g-gate",
+        "article_text_template" : "div#article_content3",
+        #"tags_template2" : "div.tag a",
+        #"tags_template": "meta[name=keywords]",
+        #"news_keywords": "meta[name=news_keywords]"
+    },
+    "www.kp.ua":
+    {
+        "articles_list_by_day_template" : "https://kp.ua/archive/{date}/",
+        "articles_list_content_blocks_template" : "ul.news-online.news-per-day",
+        "article_link_template" : "ul.news-online.news-per-day  a",
+        "date_template" : "%Y/%B/%d",
+       #"pages_template" : "div.pagination ul.pages li:last",
+        "article_title_template" : "div.content-img__main h1",
+        "article_publication_datetime" : "a.meta__date",
+        "article_text_template" : "div.content",
+        "tags_template2" : "div.related__title a",
+        #"tags_template": "meta[name=keywords]",
+        #"news_keywords": "meta[name=news_keywords]"
+    },
+     "www.unn.com.ua":
+   {
+       "articles_list_by_day_template" : "http://www.unn.com.ua/uk/news/{date}/page-{page_number}",
+       "articles_list_content_blocks_template" : "div#news_public_holder.h-news-feed", 
+       "article_link_template" : "div#news_public_holder.h-news-feed a",
+       "date_template" : "%Y/%m/%d",
+       "pages_template" : "div.b-page-selector [class!='page_nav next_page']:last",
+       "article_title_template" : "h1.title",
+       "article_publication_datetime" : "[itemprop='datePublished']",
+       "article_text_template" : "div.b-news-text.b-static-text",
+       "tags_template2" : "div.b-news-tags a", #loop through a
+       #"tags_template": ".hashtags a",
+   },
+    "zn.ua":
+   {
+       "articles_list_by_day_template" : "https://zn.ua/all-news/?page={page_number}&date={date}",
+       "articles_list_content_blocks_template" : "div.left_news_list.wide ul.news_list", 
+       "article_link_template" : "div.left_news_list.wide a.news_link",
+       "date_template" : "%Y-%m-%d",
+       "pages_template" : "div.navigate a.page:last",
+       "article_title_template" : "h1.title",
+       "article_publication_datetime" : "div.central_article .date",
+       "article_text_template" : "div.article_body .text p",
+       #"tags_template2" : "div.keywords_block", #loop through a
+       "tags_template": ".hashtags a",
+   },
+    "podrobnosti.ua":
+   {
+       "articles_list_by_day_template" : "https://podrobnosti.ua/all/archive/{date}/all/page{page_number}/", #this page does not actually load until any date is selected on the calendar
+       "articles_list_content_blocks_template" : "div.news-list.news-table",
+       "article_link_template" : "div.news-list.news-table a",
+       "date_template" : "%Y/%m/%d",
+       "pages_template" : "ul.pagination a:last",
+       "article_title_template" : "div.print_container h1",
+       "article_publication_datetime" : "div.print_container span.date",
+       "article_text_template" : "div#article_content p",
+       #"tags_template2" : "div.keywords_block", #loop through a
+       "tags_template": ".tags a",
+   }
 }
 
 
